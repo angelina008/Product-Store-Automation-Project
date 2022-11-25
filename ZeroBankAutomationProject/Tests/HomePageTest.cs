772899c9d2953ac11ec3abea228971379ad6a73e
38975 <38975@DESKTOP-M77MK7P>
2022-11-25 19:58:28 +0100

    Add project files.

---
+++ ZeroBankAutomationProject/Tests/HomePageTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroBankAutomationProject.Tests
+{
+    class HomePageTest : MainTest
+    {
+
+        [Test]
+        public void Test()
+        {
+            IWebElement button = driver.FindElement(By.Id("online-banking"));
+            button.Click();
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ ZeroBankAutomationProject/Tests/MainTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroBankAutomationProject.Tests
+{
+    class MainTest
+    {
+        string url = "http://zero.webappsecurity.com/index.html";
+        public IWebDriver driver;
+        public MainTest() { }
+
+        //public MainTest(IWebDriver driver) { this.driver = driver; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            //driver = new FirefoxDriver();
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = url;
+        }
+
+        [TearDown]
+        public void TearDown() { driver.Quit(); }
+    }
+}
