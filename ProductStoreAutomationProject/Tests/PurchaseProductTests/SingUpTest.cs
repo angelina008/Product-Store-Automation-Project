9c0943f88c26cec0b37c8a8d66ba1a397c56b86e
Angelina <angelinajanceva@hotmail.com>
2023-01-02 20:29:18 +0100

    finished Sing Up test

--- ProductStoreAutomationProject/Tests/PurchaseProductTests/SingUpTest.cs
+++ ProductStoreAutomationProject/Tests/PurchaseProductTests/SingUpTest.cs
@@ -2,17 +2,13 @@
 using ProductStoreAutomationProject.Asserts;
 using ProductStoreAutomationProject.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProductStoreAutomationProject.Tests.PurchaseProductTests
 {
     class SingUpTest : MainTest
     {
         [Test]
-        public void SingUp()
+        public void UnsuccessfulSingUp()
         {
             CarouselAsserts carouselAsserts = new(driver);
             SingUpPopUpCommands singUpPopUpCommands = new(driver);
@@ -22,29 +18,67 @@ namespace ProductStoreAutomationProject.Tests.PurchaseProductTests
 
             Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
             topMenuBarCommands.ClickOnSingUpTab();
-            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sing Up pop up is not displayed");
+            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sign up pop up is not displayed");
             singUpPopUpCommands.ClickOnCloseButton();
 
             Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
             topMenuBarCommands.ClickOnSingUpTab();
-            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sing Up pop up is not displayed");
+            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sign up pop up is not displayed");
             singUpPopUpCommands.ClickOnSingUpButton();
             Assert.That(driverCommands.ReturnMessageFromPopUp(), Is.EqualTo("Please fill out Username and Password."), "The Pop-up message is not the right one.");
             driverCommands.ClickOkOnPopUp();
 
-            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sing Up pop up is not displayed");
+            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sign up pop up is not displayed");
             singUpPopUpCommands.InsertTextInUsernameTextBox("user");
             singUpPopUpCommands.ClickOnSingUpButton();
             Assert.That(driverCommands.ReturnMessageFromPopUp(), Is.EqualTo("Please fill out Username and Password."), "The Pop-up message is not the right one.");
             driverCommands.ClickOkOnPopUp();
 
-            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sing Up pop up is not displayed");
+            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sign up pop up is not displayed");
             singUpPopUpCommands.InsertTextInUsernameTextBox("AngelinaQA");
             singUpPopUpCommands.InsertTextInPasswordTextBox("P@ssw0rd");
-            singUpPopUpCommands.ClickOnSingUpButton(); //waiting too long
+            singUpPopUpCommands.ClickOnSingUpButton();
             Assert.That(driverCommands.ReturnMessageFromPopUp(), Is.EqualTo("This user already exist."), "The Pop-up message is not the right one.");
             driverCommands.ClickOkOnPopUp();
-            //...
+            singUpPopUpCommands.ClickOnCloseButton();
+            Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
+        }
+
+        [Test]
+        [Ignore("This test is run only on clean DataBase.")]
+        public void SuccessfulSingUp()
+        {
+            CarouselAsserts carouselAsserts = new(driver);
+            SingUpPopUpCommands singUpPopUpCommands = new(driver);
+            PopUpAsserts popUpAsserts = new(driver);
+            TopMenuBarCommands topMenuBarCommands = new(driver);
+            DriverCommands driverCommands = new(driver);
+            LogInPopUpCommands logInPopUpCommands = new(driver);
+            string guidUsername = Guid.NewGuid().ToString("N").Substring(5);
+
+            Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
+            topMenuBarCommands.ClickOnSingUpTab();
+
+            Assert.That(popUpAsserts.SingUpPopUpIsDisplayed(), Is.True, "Sign up pop up is not displayed");
+            singUpPopUpCommands.InsertTextInUsernameTextBox(guidUsername);
+            singUpPopUpCommands.InsertTextInPasswordTextBox("P@ssw0rd");
+            singUpPopUpCommands.ClickOnSingUpButton();
+            Assert.That(driverCommands.ReturnMessageFromPopUp(), Is.EqualTo("Sign up successful."), "The Pop-up message is not the right one.");
+            driverCommands.ClickOkOnPopUp();
+
+            Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
+            topMenuBarCommands.ClickOnLogInTab();
+            Assert.That(popUpAsserts.LogInPopUpIsDisplayed(), Is.True, "Log In pop up is not displayed");
+            logInPopUpCommands.InsertTextInUsernameTextBox(guidUsername);
+            logInPopUpCommands.InsertTextInPasswordTextBox("P@ssw0rd");
+            logInPopUpCommands.ClickOnLogInButton();
+            Assert.That(popUpAsserts.LogInWelcomeUserIsDisplayed(), Is.True, "Welcome User is not displayed");
+            Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
+
+            topMenuBarCommands.ClickOnLogOutTab();
+            Assert.That(popUpAsserts.SingUpTabIsDisplayed(), Is.True, "Welcome User is displayed");
+            Assert.That(carouselAsserts.CarouselIsDisplayed(), Is.True, "Carousel is not displayed, Home Page is not loaded.");
+
         }
     }
 }
